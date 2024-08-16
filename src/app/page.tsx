a69a5aeb88739747a1a1c51ca8452a81c0f6c4e9
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-08-16 16:56:48 +0200

    refactor(core): form provider page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import MultiStepForm from "@/components/forms/multi-step-form";
+import { SIGNUP_FORM } from "@/constants/forms/sign-up";
 import { FormProvider } from "@/contexts/form-context";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <FormProvider>
-        <MultiStepForm />
+        <MultiStepForm formConfig={SIGNUP_FORM} />
       </FormProvider>
     </main>
   );
